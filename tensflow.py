94f69a6bf1dff1491ca753b9b649e2651ac0d555
Victor-Gmyrin <118006933+Victor-Gmyrin@users.noreply.github.com>
2022-12-24 22:15:32 +0300

    Update tensflow.py

--- tensflow.py
+++ tensflow.py
@@ -1,6 +1,13 @@
 import io
 import streamlit as st
 from PIL import Image
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.applications.efficientnet import decode_predictions
+import numpy as np
+import mathplotlib.pylot as plt
+%mathplotlib inline
 
 def load_model():
     model = EfficientNetB0(weights='imagenet')
@@ -27,13 +34,13 @@ def print_predictions(preds):
     for cl in classes:
         st.write(cl[1], cl[2])
         
-model1 = load_model()
+model = load_model()
     
 st.title('Классификация изображений')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
     x = preprocess_image(img)
-    preds = model1.predict(x)
+    preds = model.predict(x)
     st.write('**Результаты распознавания:**')
     print_predictions(preds)
